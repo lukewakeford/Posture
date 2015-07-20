44176ca15e18bb587586672363bd4af73f84ad88
Luke Wakeford <luke.wakeford@wearebase.com>
2015-07-20 21:07:15 +0100

    Build Ready.

--- src/app.js
+++ src/app.js
@@ -15,60 +15,82 @@ var z = 0;
 var sensitivity = 200;
 
 var running = false;
+var allowstart = false;
 
 var main = new UI.Window({
   fullscreen: true,
 });
 
 var set = new UI.Text({
-  position: new Vector2(0, 65),
+  position: new Vector2(5, 65),
   size: new Vector2(134, 30),
   font: 'gothic-24-bold',
-  text: 'Start/Stop >',
-  textAlign: 'right'
+  text: 'Start >',
+  textAlign: 'right',
 });
-main.add(set);
 
 var instructions = new UI.Text({
-  position: new Vector2(0, 115),
+  position: new Vector2(5, 115),
   size: new Vector2(134, 30),
   font: 'gothic-24-bold',
   text: 'Instructions >',
-  textAlign: 'right'
+  textAlign: 'right',
 });
 main.add(instructions);
+
+var sens = new UI.Text({
+  position: new Vector2(5, 15),
+  size: new Vector2(134, 30),
+  font: 'gothic-24-bold',
+  text: 'Sensitivity '+sensitivity+' >',
+  textAlign: 'right',
+});
+main.add(sens);
 main.show();
 
 main.on('click', 'up', function(e) {
-  // TODO: add a control for sensitivity 100,200,400,800
+  if (!running) {
+    if (sensitivity == 400) {
+      sensitivity = 100;
+    } else {
+      sensitivity = sensitivity+sensitivity;
+    }
+    sens.text('Sensitivity '+sensitivity+' >');
+  }
 });
 
 main.on('click', 'down', function(e) {
-  var card = new UI.Card({
-    scrollable: true
-  });
-  card.title('Instructions');
-  card.body('1. Hook your watch on the front of your collar.\n2. Sit up straight.\n3. Press the Start/Stop button.\n4. The watch will now vibrate when you slouch.');
-  card.show();
+  if (!running) {
+    var card = new UI.Card({
+      scrollable: true
+    });
+    card.title('Instructions');
+    card.body('1. Hook your watch on the front of your collar.\n2. Sit up straight.\n3. Press the Start/Stop button.\n4. The watch will now vibrate when you slouch.');
+    card.show();
+  }
 });
 
 main.on('click', 'select', function(e) {
-  UI.Vibe.vibrate('short');
-  if (running) {
-    running = false;
-  } else {
-    // TODO: add a visual indicator to show running status
-    running = true;
-    x = tempx;
-    y = tempy;
-    z = tempz;
+  if (allowstart) {
+    UI.Vibe.vibrate('short');
+    if (running) {
+      running = false;
+      main.add(instructions);
+      main.add(sens);
+      set.text('Start >');
+    } else {
+      running = true;
+      main.remove(instructions);
+      main.remove(sens);
+      set.text('Stop >');
+      x = tempx;
+      y = tempy;
+      z = tempz;
+    }
   }
 });
 
 function checkForSlouchOnAxis(value, stored){
-  
-  // TODO: add a timeout so that we don't detect slouches every second, save battery.
-  
   if (value > stored+sensitivity){
     return true;
   }
@@ -85,13 +107,13 @@ Accel.config({
   subscribe:true
 });
 Accel.on('data', function(e) {
-  
-  // TODO: wait till first accel before enabling start button.
-  
+  allowstart = true;
+  if (allowstart) {
+    main.add(set);
+  }
   tempx = e.accel.x;
   tempy = e.accel.y;
   tempz = e.accel.z;
-  
   if (running) {
     if (checkForSlouchOnAxis(tempx, x)) {
       UI.Vibe.vibrate('double');
